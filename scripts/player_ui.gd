cc343c7d4ee7903f47a4bfc1863fa54aba7bf526
Grady <arrangedgodly@gmail.com>
2024-12-03 10:40:50 -0700

    more changes

--- scripts/player_ui.gd
+++ scripts/player_ui.gd
@@ -1,25 +1,47 @@
 extends CanvasLayer
 
 @export var player: CharacterBody2D
-var active_weapon: String
+var active_weapon: Weapon
 @onready var weapons: HBoxContainer = $HBoxContainer
 
+var weapon_scenes: Dictionary = {
+	"knife": preload("res://scenes/weapons/knife.tscn").instantiate(),
+	"axe": preload("res://scenes/weapons/axe.tscn").instantiate(),
+	"pistol": preload("res://scenes/weapons/pistol.tscn").instantiate(),
+	"revolver": preload("res://scenes/weapons/revolver.tscn").instantiate(),
+	"rifle": preload("res://scenes/weapons/rifle.tscn").instantiate(),
+	"rocket_launcher": preload("res://scenes/weapons/rocket_launcher.tscn").instantiate(),
+	"shotgun": preload("res://scenes/weapons/shotgun.tscn").instantiate(),
+	"sniper": preload("res://scenes/weapons/sniper.tscn").instantiate()
+}
+
+const WEAPON_PANEL = preload("res://scenes/weapon_panel.tscn")
+
 func _ready() -> void:
-	active_weapon = "knife"
-	unlock_weapon("knife")
+	print(weapon_scenes)
+	for weapon in weapon_scenes:
+		var weapon_panel = WEAPON_PANEL.instantiate()
+		var weapon_scene = weapon_scenes[weapon]
+		weapon_panel.set_weapon(weapon_scene)
+		weapons.add_child(weapon_panel)
+		
+		
+	active_weapon = weapon_scenes["knife"]
+	unlock_weapon(active_weapon)
 	change_active_weapon(active_weapon)
+	
 	Console.add_command("unlock", unlock_weapon, 1)
 
-func change_active_weapon(new_weapon: String) -> void:
+func change_active_weapon(new_weapon: Weapon) -> void:
 	for weapon in weapons.get_children():
-		if weapon.weapon_type == new_weapon:
+		if weapon == new_weapon:
 			weapon.set_selection(true)
 		else:
 			weapon.set_selection(false)
 
-func unlock_weapon(new_weapon: String) -> void:
+func unlock_weapon(new_weapon: Weapon) -> void:
 	for weapon in weapons.get_children():
-		if weapon.weapon_type == new_weapon:
+		if weapon == new_weapon:
 			weapon.unlock()
 
 func _exit_tree() -> void:
--- scripts/weapon_panel.gd
+++ scripts/weapon_panel.gd
@@ -3,31 +3,20 @@ extends TextureRect
 const NON_SELECTED = preload("res://assets/Updated_ZombiePack_AssetPack/UI/panel 2 (32x32).png")
 const SELECTED = preload("res://assets/Updated_ZombiePack_AssetPack/UI/panel 1 (32x32).png")
 
-@onready var sprite: Sprite2D = $Sprite2D
 @onready var locked_shader = preload("res://shaders/locked.tres").duplicate()
-
-@export_enum("pistol", "shotgun", "rifle", "sniper", "axe", "rocket_launcher", "knife", "revolver") var weapon_type: String = "knife"
+var weapon
 
 var is_unlocked: bool = false
 
-var weapon_frames: Dictionary = {
-	"sniper": 0,
-	"pistol": 1,
-	"revolver": 2,
-	"shotgun": 3,
-	"axe": 6,
-	"rifle": 7,
-	"rocket_launcher": 8,
-	"knife": 9
-}
-
 func _ready() -> void:
 	self.texture = NON_SELECTED
+	
+func set_weapon(new_weapon: Weapon) -> void:
+	weapon = new_weapon
 	var rect_size = self.size
-	sprite.position = rect_size / 2
-	sprite.frame = weapon_frames[weapon_type]
-	sprite.scale = Vector2(2, 2)
-	sprite.material = locked_shader
+	weapon.position = rect_size / 2
+	weapon.scale = Vector2(2, 2)
+	weapon.material = locked_shader
 
 func set_selection(new_selection: bool) -> void:
 	if !is_unlocked:
@@ -39,5 +28,5 @@ func set_selection(new_selection: bool) -> void:
 		self.texture = NON_SELECTED
 
 func unlock() -> void:
-	sprite.material = null
+	weapon.material = null
 	is_unlocked = true
